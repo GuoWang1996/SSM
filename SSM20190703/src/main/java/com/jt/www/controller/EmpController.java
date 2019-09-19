e2abce7d59bd301e33679a368a81c5b68b77a60f
gw <2431605485@qq.com>
2019-09-19 11:11:24 +0800

    分支22

--- SSM20190703/src/main/java/com/jt/www/controller/EmpController.java
+++ SSM20190703/src/main/java/com/jt/www/controller/EmpController.java
@@ -80,7 +80,7 @@ public class EmpController {
 	   public void testInsert() throws Exception{           
 		  Emp e=new Emp();
 		  e.setEmpno(4);
-		  e.setEname("马六");
+		  e.setEname("马六分支2");
 		  e.setComm(2);
 		  e.setJob("程序员");
 		  e.setMgr(2);
